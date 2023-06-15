faf7d8bd9ecc3c480f1377885c6bae7ed157d394
Kurzhalsgiraffe <max-hantschke@web.de>
2023-06-15 20:53:19 +0200

    added user class

--- BlockchainProjekt/src/main/java/client/Main.java
+++ BlockchainProjekt/src/main/java/client/Main.java
@@ -2,49 +2,42 @@ package client;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 import dao.BlockValidator;
-import dao.BlockchainuserDao;
 import dao.InitBlockchainManager;
 import dao.InitBlockchainManagerMiner;
 import dao.InitializationAlreadyDoneException;
 import dao.MyBlockchainuserKeysDao;
+import dao.NoEntityFoundException;
 import dao.NoSuchRowException;
+import dao.SaveException;
+import dao.TargetListNotEmptyException;
 import model.block.Block;
 
 public class Main {
-	private final static int INITBASE_MINER = 4096;
-	private final static int INITBASE_USER = 1024;
+	public final static int INITBASE_MINER = 4096;
+	public final static int INITBASE_USER = 1024;
 	
 	private final static String miner = "minerProjektVS_SS23";          
 	private final static String minerPassword = "minerProjektVS_SS23";
-
-	private final static String firstUserName = "hantscma";
-	private final static String firstUserPassword = "hantscma";
-	private final static String firstUserChoice = "Gruene";
-
-	private final static String secondUserName = "rothnina";
-	private final static String secondUserPassword = "rothnina";
-	private final static String secondUserChoice = "Tierschutzpartei";
-
-	private final static String thirdUserName = "heinzelu";
-	private final static String thirdUserPassword = "heinzelu";
-	private final static String thirdUserChoice = "CDU";
-
 	private final static MyBlockManager blockManagerMiner = new MyBlockManager("BlockchainMiner", miner, minerPassword);
-	private final static MyBlockManager blockManagerFirstUser = new MyBlockManager("FirstUser", firstUserName, firstUserPassword);
-	private final static MyBlockManager blockManagerSecondUser = new MyBlockManager("SecondUser", secondUserName, secondUserPassword);
+	
+	private final static User firstUser = new User("FirstUser", "hantscma", "hantscma", "Gruene");
+	private final static User secondUser = new User("SecondUser", "rothnina", "rothnina", "Tierschutzpartei");
+	private final static User thirdUser = new User("ThirdUser", "heinzelu", "heinzelu", "CDU");
 
 	public static void usage() {
 		System.out.println("Usage: java Main EinrichtenMiner");			// Schritt 0
 		System.out.println("Usage: java Main ZulassungUser");			// Schritt 1
 		System.out.println("Usage: java Main LoginErsterBenutzer");		// Schritt 2
-		System.out.println("Usage: java Main LoginZweiterBenutzer");	// Schritt 3
+		System.out.println("Usage: java Main VoteZweiterBenutzer");	// Schritt 3
 		System.out.println("Usage: java Main Read");					// beliebig nach Schritt 2
 		System.out.println("Usage: java Main Validate");				// beliebig nach Schritt 2
 		System.out.println("Usage: java Main EvaluateElection");		// beliebig nach Schritt 2
@@ -64,89 +57,17 @@ public class Main {
 
 //ZulassungUser
 			} else if (args[0].equals("ZulassungUser")) {
-				permitUser(firstUserName, firstUserPassword);
-				permitUser(secondUserName, secondUserPassword);
-				permitUser(thirdUserName, thirdUserPassword);
+				firstUser.permit();
+				secondUser.permit();
+				thirdUser.permit();
 
-//LoginErsterBenutzer
-			} else if (args[0].equals("LoginErsterBenutzer")) {			
+//VoteErsterBenutzer
+			} else if (args[0].equals("VoteErsterBenutzer")) {
+				vote(firstUser);
 
-				// Überprüfe ob der User zur Wahl zugelassen ist
-				if (userEligible(firstUserName, firstUserPassword)) {
-					System.out.println(firstUserName + " ist zur Wahl zugelassen");
-					
-					// Überprüfe ob User bereits ein Keypair erzeugt hat
-					if (!userHasPublicKey(firstUserName)) {
-						System.out.println(firstUserName + " hat keinen Public Key, versuche ihn anzulegen");
-						initUser("SecondUser", firstUserName, firstUserPassword);
-					}
-
-					List<Block> myBlockList = blockManagerMiner.getBlockListFromId(blockManagerFirstUser.getIdFromLastBlock());
-					for (Block b : myBlockList) {
-						System.out.println("block = " + b);
-					}
-					
-					InitBlockchainManager bc2User = getUser("FirstUser", firstUserName, firstUserPassword);
-					byte[] encryptedUser = RSA.encrypt(firstUserName.getBytes(), bc2User.getMyKeys().getPublickey(), INITBASE_USER);
-
-					if (blockManagerMiner.checkIfUserHasVoted(encryptedUser)) {
-						System.out.println(firstUserName + " hat schon gewählt. Wahl wurde nicht übernommen");
-					} else {
-						// Generiere Block aus dem verschlüsselten Usernamen und dessen Wahl
-						byte[] blockData = generateBlockData(encryptedUser, firstUserChoice);
-						byte[] encryptedBlockData = RSA.encrypt(blockData, getMinerPublicKey(), INITBASE_MINER);
-						
-						// Erzeuge Block und weise ihm eine ID zu
-						Block block = new Block(encryptedBlockData, 0);
-						block.setId(blockManagerMiner.calculateNextId());
-
-						// Speichere den Blocks auf der DB des Miners
-						blockManagerMiner.append(block);
-						blockManagerFirstUser.copyList(blockManagerMiner.getBlockListFromId(blockManagerFirstUser.getIdFromLastBlock()));
-					}
-				} else {
-					System.out.println("Fehler: " + firstUserName + " ist nicht zur Wahl zugelassen!");
-				}
-
-//LoginZweiterBenutzer
-			} else if (args[0].equals("LoginZweiterBenutzer")) {
-				
-				// Überprüfe ob der User zur Wahl zugelassen ist
-				if (userEligible(secondUserName, secondUserPassword)) {
-					System.out.println(secondUserName + " ist zur Wahl zugelassen");
-					
-					// Überprüfe ob User bereits ein Keypair erzeugt hat
-					if (!userHasPublicKey(secondUserName)) {
-						System.out.println(secondUserName + " hat keinen Public Key, versuche ihn anzulegen");
-						initUser("SecondUser", secondUserName, secondUserPassword);
-					}
-
-					List<Block> myBlockList = blockManagerMiner.getBlockListFromId(blockManagerSecondUser.getIdFromLastBlock());
-					for (Block b : myBlockList) {
-						System.out.println("block = " + b);
-					}
-					
-					InitBlockchainManager bc2User = getUser("SecondUser", secondUserName, secondUserPassword);
-					byte[] encryptedSecondUser = RSA.encrypt(secondUserName.getBytes(), bc2User.getMyKeys().getPublickey(), INITBASE_USER);
-
-					if (blockManagerMiner.checkIfUserHasVoted(encryptedSecondUser)) {
-						System.out.println(secondUserName + " hat schon gewählt. Wahl wurde nicht übernommen");
-					} else {
-						// Generiere Block aus dem verschlüsselten Usernamen und dessen Wahl
-						byte[] blockData = generateBlockData(encryptedSecondUser, secondUserChoice);
-						byte[] encryptedBlockData = RSA.encrypt(blockData, getMinerPublicKey(), INITBASE_MINER);
-						
-						// Erzeuge Block und weise ihm eine ID zu
-						Block block = new Block(encryptedBlockData, 0);
-						block.setId(blockManagerMiner.calculateNextId());
-
-						// Speichere den Blocks auf der DB des Miners
-						blockManagerMiner.append(block);
-						blockManagerSecondUser.copyList(blockManagerMiner.getBlockListFromId(blockManagerSecondUser.getIdFromLastBlock()));
-					}
-				} else {
-					System.out.println("Fehler: " + secondUserName + " ist nicht zur Wahl zugelassen!");
-				}
+//VoteZweiterBenutzer
+			} else if (args[0].equals("VoteZweiterBenutzer")) {
+				vote(secondUser);
 
 //Read
 			} else if (args[0].equals("Read")) {
@@ -167,7 +88,7 @@ public class Main {
 				System.out.println("---------------------------------");
 				System.out.println("---------------------------------");
 				System.out.println("Ausgabe der Blockliste des Users mit Beschraenkung auf Daten");
-				for (Block obj : blockManagerFirstUser.list()) {
+				for (Block obj : firstUser.blockManager.list()) {
 					System.out.println("\nDatum als Bytearray = " + Arrays.toString(obj.getDataAsObject()));
 					System.out.println("von Bytearray zurueckkonvertiertes Datum als Text = " + new String(obj.getDataAsObject(), StandardCharsets.UTF_8));
 				}
@@ -200,7 +121,6 @@ public class Main {
 					// Ergebnis:
 					// 1. 1, falls Wähler Stimme abgegeben hat
 					// 2. 0, falls Wähler nicht gewählt hat
-
 				}
 			} else {
 				usage();
@@ -217,36 +137,11 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
-
-	public static void initUser(String persistanceUnit, String username, String userPassword) throws NoSuchRowException {
-
-		InitBlockchainManager initForUser = new InitBlockchainManager(persistanceUnit, username, userPassword);
-		try {
-			initForUser.initDatabase(); // initialisiere Datenbank fuer User
-			initForUser.initKeys(INITBASE_USER);
-		} catch (InitializationAlreadyDoneException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void permitUser(String username, String userPassword) throws NoSuchRowException {
-		BlockchainuserDao b1 = new BlockchainuserDao();
-		b1.save(username, userPassword);
-	}
-
-	public static boolean userEligible(String username, String userPassword) throws NoSuchRowException {
-		BlockchainuserDao b1 = new BlockchainuserDao();
-		return b1.userEligible(username, userPassword);
-	}
-
-	public static boolean userHasPublicKey(String username) throws NoSuchRowException {
-		BlockchainuserDao b1 = new BlockchainuserDao();
-		return b1.userHasPublicKey(username);
-	}
-
-	public static InitBlockchainManager getUser(String persistanceUnit, String username, String userPassword) throws NoSuchRowException {
-		InitBlockchainManager manager = new InitBlockchainManager(persistanceUnit, username, userPassword);
-		return manager;
+	
+	public static PublicKey getMinerPublicKey() throws NoSuchRowException {
+		MyBlockchainuserKeysDao bcuK = new MyBlockchainuserKeysDao();
+		PublicKey publicKeyMiner = bcuK.getMyKeys().getPublickey();
+		return publicKeyMiner;
 	}
 	
 	public static byte[] generateBlockData(byte[] encryptedUser, String userChoice) {
@@ -262,10 +157,47 @@ public class Main {
 		byte[] blockData = outputStream.toByteArray();
 		return blockData;
 	}
+	
+	public static void vote(User user) {
+		// Überprüfe ob der User zur Wahl zugelassen ist
+		try {
+			if (user.isEligible()) {
+				System.out.println(user.username + " ist zur Wahl zugelassen");
+				
+				// Überprüfe ob User bereits ein Keypair erzeugt hat
+				if (!user.hasPublicKey()) {
+					System.out.println(user.username + " hat keinen Public Key, versuche ihn anzulegen");
+					user.init();
+				}
 
-	public static PublicKey getMinerPublicKey() throws NoSuchRowException {
-		MyBlockchainuserKeysDao bcuK = new MyBlockchainuserKeysDao();
-		PublicKey publicKeyMiner = bcuK.getMyKeys().getPublickey();
-		return publicKeyMiner;
+				List<Block> myBlockList = blockManagerMiner.getBlockListFromId(user.blockManager.getIdFromLastBlock());
+				for (Block b : myBlockList) {
+					System.out.println("block = " + b);
+				}
+
+				InitBlockchainManager bc2User = new InitBlockchainManager(user.persistanceUnit, user.username, user.password);
+				byte[] encryptedSecondUser = RSA.encrypt(user.username.getBytes(), bc2User.getMyKeys().getPublickey(), INITBASE_USER);
+
+				if (blockManagerMiner.checkIfUserHasVoted(encryptedSecondUser)) {
+					System.out.println(user.username + " hat schon gewählt. Wahl wurde nicht übernommen");
+				} else {
+					// Generiere Block aus dem verschlüsselten Usernamen und dessen Wahl
+					byte[] blockData = generateBlockData(encryptedSecondUser, user.choice);
+					byte[] encryptedBlockData = RSA.encrypt(blockData, getMinerPublicKey(), INITBASE_MINER);
+					
+					// Erzeuge Block und weise ihm eine ID zu
+					Block block = new Block(encryptedBlockData, 0);
+					block.setId(blockManagerMiner.calculateNextId());
+
+					// Speichere den Blocks auf der DB des Miners
+					blockManagerMiner.append(block);
+					user.blockManager.copyList(blockManagerMiner.getBlockListFromId(user.blockManager.getIdFromLastBlock()));
+				}
+			} else {
+				System.out.println("Fehler: " + user.username + " ist nicht zur Wahl zugelassen!");
+			}
+		} catch (NoSuchAlgorithmException | UnsupportedEncodingException | NoSuchRowException | InitializationAlreadyDoneException | SaveException | NoEntityFoundException | TargetListNotEmptyException e) {
+			e.printStackTrace();
+		}
 	}
 }
---
+++ BlockchainProjekt/src/main/java/client/User.java
@@ -0,0 +1,43 @@
+package client;
+
+import dao.BlockchainuserDao;
+import dao.InitBlockchainManager;
+import dao.InitializationAlreadyDoneException;
+import dao.NoSuchRowException;
+
+public class User {
+	public String persistanceUnit;
+	public String username;
+	public String password;
+	public String choice;
+	public MyBlockManager blockManager;
+	
+	public User(String persistanceUnit, String username, String password, String choice) {
+		this.username = username;
+		this.password = password;
+		this.persistanceUnit = persistanceUnit;
+		this.choice = choice;
+		this.blockManager = new MyBlockManager(persistanceUnit, username, password);
+	}
+	
+	public void permit() throws NoSuchRowException {
+		BlockchainuserDao b = new BlockchainuserDao();
+		b.save(this.username, this.password);
+	}
+	
+	public boolean isEligible() throws NoSuchRowException {
+		BlockchainuserDao b = new BlockchainuserDao();
+		return b.userEligible(this.username, this.password);
+	}
+	
+	public boolean hasPublicKey() throws NoSuchRowException {
+		BlockchainuserDao b = new BlockchainuserDao();
+		return b.userHasPublicKey(this.username);
+	}
+	
+	public void init() throws NoSuchRowException, InitializationAlreadyDoneException {
+		InitBlockchainManager initForUser = new InitBlockchainManager(this.persistanceUnit, this.username, this.password);
+		initForUser.initDatabase();
+		initForUser.initKeys(Main.INITBASE_USER);
+	}
+}
